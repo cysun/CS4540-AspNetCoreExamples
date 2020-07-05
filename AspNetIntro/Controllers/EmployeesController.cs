f8ec3e9c6ea18573953af5c7d8638d553edefc1a
Chengyu Sun <csun@calstatela.edu>
2020-07-05 11:20:42 -0700

    Add project files.

---
+++ AspNetIntro/Controllers/EmployeesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetIntro.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetIntro.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private readonly IEmployeeService _employeeService;
+
+        public EmployeesController(IEmployeeService employeeService)
+        {
+            _employeeService = employeeService;
+        }
+
+        public IActionResult List()
+        {
+            return View(_employeeService.GetEmployees());
+        }
+    }
+}
