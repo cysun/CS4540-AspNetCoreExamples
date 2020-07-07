1a3404ce76377f7bde65fae188ea6570701ac9cb
Chengyu Sun <csun@calstatela.edu>
2020-07-07 10:09:34 -0700

    Added AspNetMVC Example

---
+++ AspNetMVC/Controllers/EmployeesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetMVC.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetMVC.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private readonly IEmployeeService _employeeService;
+
+        public EmployeesController(IEmployeeService employeeService)
+        {
+            _employeeService = employeeService;
+        }
+
+        public IActionResult List()
+        {
+            return View(_employeeService.GetEmployees());
+        }
+    }
+}
---
+++ AspNetMVC/Services/EmployeeService.cs
@@ -0,0 +1,27 @@
+﻿using AspNetMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetMVC.Services
+{
+    public interface IEmployeeService
+    {
+        List<Employee> GetEmployees();
+    }
+
+    public class MockEmployeeService : IEmployeeService
+    {
+        private List<Employee> employees = new List<Employee>
+        {
+            new Employee{ Id = 1, Name = "John"},
+            new Employee{ Id = 2, Name = "Jane"},
+        };
+
+        public List<Employee> GetEmployees()
+        {
+            return employees;
+        }
+    }
+}
