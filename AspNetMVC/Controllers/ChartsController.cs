0956f39920031618609f6c933387fbd781534bfd
Chengyu Sun <csun@calstatela.edu>
2020-07-17 18:25:37 -0700

    Added chart examples

---
+++ AspNetMVC/Controllers/ChartsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetMVC.Controllers
+{
+    public class ChartsController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Dates = new List<string> { "6/7/2020", "6/8/2020", "6/9/2020", "6/10/2020" };
+            return View(new List<int> { 10, 12, 15, 20 });
+        }
+    }
+}
